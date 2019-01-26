b28e59beacaaa4541796e15b4605be9f7194fbbe
peter <phwang@evolenthealth.com>
2019-01-26 14:52:33 -0600

    Update test cases

--- test_agecalc.py
+++ test_agecalc.py
@@ -4,18 +4,14 @@ import pytest
 import agecalc
 
 
-def test_calculate():
+def test_calculate_exact_month_day():
     assert agecalc.calculate(date(2000, 1, 1), date(2010, 1, 1)) == 10
-    assert agecalc.calculate(date(2000, 1, 1), date(2010, 1, 2)) == 10
 
-def test_calculate_different_months_days():
-    assert agecalc.calculate(date(2000, 7, 1), date(2010, 6, 30)) == 9
+def test_calculate_greater_month_day():
+    assert agecalc.calculate(date(2000, 1, 1), date(2010, 2, 1)) == 10
 
-def test_calculate_present_date_equals_birthdate():
-    birthdate = date(2000, 1, 1)
-    evaldate = date(2000, 1, 1)
-
-    assert agecalc.calculate(birthdate, evaldate) == 0
+def test_calculate_smaller_month_day():
+    assert agecalc.calculate(date(2000, 1, 2), date(2010, 1, 1)) == 9
 
 def test_calculate_present_date_less_than_birthdate_raises_valueerror():
     birthdate = date(2000, 1, 1)
