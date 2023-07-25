1dd14a2ceaee768dfb40a15b43b9baf699b46fc8
LujingCai <cailujing@foxmail.com>
2023-07-25 14:30:02 +0100

    make them functional equal

--- features/steps/gpt-steps.py
+++ features/steps/gpt-steps.py
@@ -38,3 +38,31 @@
 # def step_then_it_should_raise_value_error(context):
 #     assert isinstance(context.error, ValueError), \
 #         'Expected ValueError, but got none'
+
+
+# from behave import given, when, then
+# from datetime import datetime
+# from agecalc import calculate
+#
+#
+# @given('birthdate "{birthdate}" and evaldate "{evaldate}"')
+# def step_given_birthdate_and_evaldate(context, birthdate, evaldate):
+#     context.birthdate = datetime.strptime(birthdate, "%Y-%m-%d").date()
+#     context.evaldate = datetime.strptime(evaldate, "%Y-%m-%d").date()
+#
+# @when('invoked')
+# def step_when_invoked(context):
+#     try:
+#         context.result = calculate(context.birthdate, context.evaldate)
+#     except ValueError as e:
+#         context.error = e
+#
+# @then('the result should be equal to "{expected_age}"')
+# def step_then_result_should_be_equal_to(context, expected_age):
+#     assert context.result == int(expected_age), \
+#         f"Expected {expected_age}, but got {context.result}"
+#
+# @then('it should raise ValueError')
+# def step_then_it_should_raise_ValueError(context):
+#     assert isinstance(context.error, ValueError), \
+#         "Expected ValueError, but got different error"
